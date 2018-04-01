85346fd2b28cd6803038513fc90b19fadf4ef77b
Sior <aho@sior.be>
2018-04-01 18:34:06 +0200

    FUCKING INCONSISTENCY

--- index.js
+++ index.js
@@ -167,8 +167,9 @@ function main() {
         if (message.content.startsWith(ConfigUtils.getconfig().Prefix) && message.author.id != client.user.id) {
             var msg = message.content.substring(ConfigUtils.getconfig().Prefix.length);
             var args = msg.split(" ");
-            if (CommandManager.getAliases()[args[0].toUpperCase()]) {
-                var replacement = CommandManager.getAliases()[args[0].toUpperCase()];
+            var alias = CommandManager.getAliases()[args[0].toUpperCase()];
+            if (alias) {
+                var replacement = alias;
                 msg = CommandManager.getAliases()[args[0].toUpperCase()] + msg.substring(args[0].length);
                 args[0] = replacement;
             }
