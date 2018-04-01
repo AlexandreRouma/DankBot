56f2eba73d054fdfb249a528071d13bf63902e98
Sior <aho@sior.be>
2018-04-01 20:38:09 +0200

    Added a semicolon

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ function main() {
         if (message.content.startsWith(ConfigUtils.getconfig().Prefix) && message.author.id != client.user.id) {
             var msg = message.content.substring(ConfigUtils.getconfig().Prefix.length);
             var args = msg.split(" ");
-            var alias = CommandManager.getAliases()[args[0].toUpperCase()]
+            var alias = CommandManager.getAliases()[args[0].toUpperCase()];
             if (alias) {
                 msg = alias + msg.substring(args[0].length);
                 args[0] = alias;
