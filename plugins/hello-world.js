7b28822310ef337f118a71a6b19732a42da050e4
AlexandreRouma <alexandre.rouma@gmail.com>
2018-04-01 21:26:16 +0200

    renamed example plugin

--- plugins/hello-world.js
+++ plugins/hello-world.js
@@ -3,11 +3,11 @@ var CommandManager = require("../command-manager");
 var client;
 
 module.exports._plugin_info = { // All of these need to be present, but doesn't need to be filled in (except if "REQUIRED")
-    name: "helloworld",                                      // Plugin name (REQUIRED)
+    name: "hello-world",                                      // Plugin name (REQUIRED)
     author: "xX_WhatsTheGeek_Xx",                            // Plugin author 
     url: "https://github.com/AlexandreRouma/DankBot",        // Plugin URL (github/wiki/info page/etc...)
     version: "1.0",                                          // Plugin version
-    description: "Test plugin !",                            // Plugin description
+    description: "Hello World plugin !",                            // Plugin description
     additional_info: {                                       // Additional info about the plugin (doesn't have to contain anything)
         what_you_want_here: "hey !"
     }
@@ -15,6 +15,7 @@ module.exports._plugin_info = { // All of these need to be present, but doesn't 
 
 module.exports._load = function (_client) {
     client = _client;
+    //                                 Name     Alias     Usage          Description
     CommandManager.registerCommand("HELLOWORLD", "HW", "helloworld", "Say 'Hello World' !", false, helloworld);
 }
 
