cfd1d77510af2b01cb993375557e7a40a1133df0
AlexandreRouma <alexandre.rouma@gmail.com>
2018-04-07 17:13:36 +0200

    fixed comments again

--- plugins/hello-world.js
+++ plugins/hello-world.js
@@ -3,11 +3,11 @@ var PluginManager = require("../plugin-manager");
 var client;
 
 var _plugin_info = { // All of these need to be present, but doesn't need to be filled in (except if "REQUIRED")
-    name: "hello-world",                                      // Plugin name (REQUIRED)
+    name: "hello-world",                                     // Plugin name (REQUIRED)
     author: "xX_WhatsTheGeek_Xx",                            // Plugin author 
     url: "https://github.com/AlexandreRouma/DankBot",        // Plugin URL (github/wiki/info page/etc...)
     version: "1.0",                                          // Plugin version
-    description: "Hello World plugin !",                            // Plugin description
+    description: "Hello World plugin !",                     // Plugin description
     additional_info: {                                       // Additional info about the plugin (doesn't have to contain anything)
         what_you_want_here: "hey !"
     }
